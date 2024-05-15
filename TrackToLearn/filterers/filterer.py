dd5e6e9935f872da4bd2ed7c9bf0d7343e1a51e0
Jeremi Levesque <jeremilevesque@hotmail.com>
2024-05-15 10:27:22 -0400

    RLHF progress

--- TrackToLearn/filterers/filterer.py
+++ TrackToLearn/filterers/filterer.py
@@ -1,12 +1,14 @@
 from abc import abstractmethod, ABCMeta
+from dipy.io.stateful_tractogram import StatefulTractogram
 
-class TractsFilterer(metaclass=ABCMeta):
+class Filterer(metaclass=ABCMeta):
     
     def __init__(self):
         pass
 
     @abstractmethod
-    def filter(self, tractogram):
+    def __call__(self, reference, tractograms, gt_config, out_dir, scored_extension="trk", tmp_base_dir=None):
         """Filter a list of tracts."""
         raise NotImplementedError
+    
     
