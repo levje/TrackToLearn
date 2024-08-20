00653414e566084651a6aee5520cad63fc62781f
Jeremi Levesque <jeremilevesque@hotmail.com>
2024-08-20 16:05:40 -0400

    Add ppo search script

---
+++ ismrm_ttl_ppo_for_search.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=40000M
+#SBATCH --time=20:00:00
+#SBATCH --mail-user=jeremi.levesque@usherbrooke.ca
+#SBATCH --mail-type=ALL
+
+# The above comments are used by SLURM to set the job parameters.
+set -e
+
+# SEARCH PARAMETERS
+LR=$1
+GAMMA=$2
+POLICYCLIP=$3
+
+# Set this to 0 if running on a cluster node.
+islocal=1
+RUN_OFFLINE=0
+
+# Expriment parameters
+EXPNAME="ClassicPPO-Oracle-Search"
+COMETPROJECT="ClassicPPO-Oracle-Search"
+EXPID="Base_wOracle_"_$(date +"%F-%H_%M_%S")
+# RLHFINTERNPV=20         # Number of seeds per tractogram generated during the RLHF pipeline
+MAXEP=1000                # Number of PPO iterations
+# ORACLENBSTEPS=10        # Number of steps for the oracle
+# AGENTNBSTEPS=100        # Number of steps for the agent
+# PRETRAINSTEPS=1000      # Number of steps for pretraining if no agent checkpoint is provided.
+
+NPV=20
+SEEDS=(1111)
+BATCHSIZE=4096
+THETA=30
+
+if [ $islocal -eq 1 ]; then
+    # This script should be ran from the root of the project is ran locally.
+    echo "Running training locally..."
+    SOURCEDIR=.
+    DATADIR=data/datasets/ismrm2015_2mm
+    EXPDIR=data/experiments
+    LOGSDIR=data/logs
+
+    # If CONDAENV is not set, PYTHON EXEC should be python, else it should be the python executable of the conda environment.
+    if [ -z $1 ]; then
+        PYTHONEXEC=python
+        echo "WARNING: No conda environment provided. Using the environment loaded when calling the script."
+    else
+        PYTHONEXEC=~/miniconda3/envs/$1/bin/python
+    fi
+    DATASETDIR=$DATADIR
+    #ORACLECHECKPOINT=custom_models/ismrm_paper_oracle/ismrm_paper_oracle.ckpt
+else
+    echo "Running training on a cluster node..."
+    module load python/3.10 cuda cudnn httpproxy
+    SOURCEDIR=~/TrackToLearn
+    DATADIR=$SLURM_TMPDIR/data
+    EXPDIR=$SLURM_TMPDIR/experiments
+    LOGSDIR=$SLURM_TMPDIR/logs
+    PYTHONEXEC=python
+    export COMET_API_KEY=$(cat ~/.comet_api_key)
+
+    ORACLECHECKPOINT=$DATADIR/ismrm_paper_oracle.ckpt
+
+    # Prepare virtualenv
+    echo "Sourcing ENV-TTL-2 virtual environment..."
+    source ~/ENV-TTL-2/bin/activate # Ideally, we would build the environnement within the node itself, but too much dependencies for now.
+
+    # Prepare datasets
+    mkdir $DATADIR
+    mkdir $EXPDIR
+    mkdir $LOGSDIR
+
+    echo "Unpacking datasets..."
+    tar xf ~/projects/def-pmjodoin/levj1404/datasets/ismrm2015_2mm_ttl.tar.gz -C $DATADIR
+    DATASETDIR=$DATADIR/ismrm2015_2mm
+
+    echo "Copying oracle checkpoint..."
+    cp ~/projects/def-pmjodoin/levj1404/oracles/ismrm_paper_oracle.ckpt $DATADIR
+fi
+
+for RNGSEED in "${SEEDS[@]}"
+do
+    DEST_FOLDER="${EXPDIR}/${EXPNAME}/${EXPID}"
+
+    additionnal_args=()
+    # if [ $RUN_OFFLINE -eq 1 ]; then
+    #     additionnal_args+=('--comet_offline_dir' "${LOGSDIR}")
+    # fi
+    # if [ -n "$AGENTCHECKPOINT" ]; then
+    #     additionnal_args+=('--agent_checkpoint' "${AGENTCHECKPOINT}")
+    # fi
+
+    # Start training
+    ${PYTHONEXEC} \
+        $SOURCEDIR/TrackToLearn/trainers/classic_ppo_train.py \
+        ${DEST_FOLDER} \
+        "${COMETPROJECT}" \
+        "${EXPID}" \
+        "${DATASETDIR}/ismrm2015.hdf5" \
+        --workspace "mrzarfir" \
+        --hidden_dims "1024-1024-1024" \
+        --use_comet \
+        --n_actor 4096 \
+        --min_length 20 \
+        --max_length 200 \
+        --noise 0.0 \
+        --binary_stopping_threshold 0.1 \
+        --oracle_bonus 0.0 \
+        --alignment_weighting 1.0 \
+        --scoring_data "${DATASETDIR}/scoring_data" \
+        --tractometer_reference "${DATASETDIR}/scoring_data/t1.nii.gz" \
+        --tractometer_validator \
+        --rng_seed ${RNGSEED} \
+        --npv ${NPV} \
+        --lr ${LR} \
+        --gamma ${GAMMA} \
+        --theta ${THETA} \
+        --n_dirs 100 \
+        --max_ep ${MAXEP} \
+        --entropy_loss_coeff 0.001 \
+        --lmbda 0.95 \
+        --eps_clip ${POLICYCLIP} \
+        --K_epochs 30 \
+        --action_std 0.0 \
+        "${additionnal_args[@]}"
+        # --use_classic_reward \
+
+    # POST-PROCESSING
+    bash scripts/tractogram_post_processing.sh ${DEST_FOLDER} ${DATASETDIR}
+done
+
+if [ $islocal -eq 1 ]; then
+    echo "Experiment results are saved in ${EXPDIR}."
+    echo "To see the results on Comet.ml, please run \"comet upload ${LOGSDIR}/<comet-exp-hash>.zip\"."
+    echo "Done."
+else
+    # Archive and save everything
+    OUTNAME=${EXPID}$(date -d "today" +"%Y%m%d%H%M").tar
+
+    echo "Archiving experiment..."
+    tar -cvf ${DATADIR}/${OUTNAME} $EXPDIR
+    echo "Copying archive to scratch..."
+    cp ${DATADIR}/${OUTNAME} ~/scratch/${OUTNAME}
+fi
---
+++ ppo_search_launcher.sh
@@ -0,0 +1,55 @@
+# Description: This script is used to launch a hyperparameter search for the PPO algorithm.
+be_safe=1   # Set to 1 to avoid launching jobs by mistake. Set at 0 at your own risk of
+            # launching a lot of jobs.
+
+lrs=(0.00001 0.00005 0.0005)
+gammas=(0.5 0.75 0.90 0.95)
+clips=(0.1)
+
+# Count the number of process to launch
+n_jobs=$(( ${#lrs[@]} * ${#gammas[@]} * ${#clips[@]} ))
+
+# If the n_jobs is too high, exit the script.
+if [[ $n_jobs -gt 20 ]]; then
+    echo "The number of jobs to launch is too high (${n_jobs} > 20)."
+    echo "Please reduce the number of hyperparameters to search."
+    exit 1
+fi
+
+# Print the number of jobs to launch and prompt the user to continue. If the user does not
+# wish to continue, stop the script.
+echo "${n_jobs} jobs will be launched."
+read -p "Continue? [y/n] " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 1
+fi
+
+# Iterate over each list of hyperparameters and for each possible combination, launch a new job
+# by calling dummy_script.sh with the hyperparameters as arguments.
+job_id=1
+accept_all=0
+for lr in "${lrs[@]}"; do
+    for gamma in "${gammas[@]}"; do
+        for clip in "${clips[@]}"; do
+            # If be_safe is set to 1, ask the user to confirm the launch of the job.
+            if [[ $be_safe -eq 1 ]]; then
+                read -p "Launch job with lr=${lr}, gamma=${gamma}, clip=${clip}? [y/n] " -n 1 -r
+                echo
+                
+                if [[ $REPLY =~ ^[Aa]$ ]]; then
+                    echo "Launching all jobs."
+                    accept_all=1
+                elif [[ ! $REPLY =~ ^[Yy]$ ]]; then
+                    echo "Job not launched."
+                    exit 1
+                fi
+            else
+                echo "${job_id}/${n_jobs} => Queuing job with lr=${lr}, gamma=${gamma}, clip=${clip}."
+            fi
+
+            sbatch ismrm_ttl_ppo_for_search.sh $lr $gamma $clip
+            job_id=$((job_id+1))
+        done
+    done
+done
